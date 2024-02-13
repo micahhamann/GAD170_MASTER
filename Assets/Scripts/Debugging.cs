f99d9b39797d34f48ce1241723eaa87cc301b1a0
micahhamann <1017044@student.sae.edu.au>
2024-02-13 15:07:51 +1030

    commit

---
+++ Assets/Scripts/Debugging.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugging : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Heeey");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
